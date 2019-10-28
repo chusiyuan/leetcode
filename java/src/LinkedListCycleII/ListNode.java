5c0bc17e0462eb8b672f6f0e65932b8a8ad92764
chusiyuan <chusiyuan96@gmail.com>
2019-10-28 22:56:43 +0800

    change the directory structure

---
+++ java/src/LinkedListCycleII/ListNode.java
@@ -0,0 +1,11 @@
+package LinkedListCycleII;
+
+//Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
